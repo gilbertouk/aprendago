54dfd3c76dfe63bfcc92618a4d8525234deacf85
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-08-12 15:21:28 +0100

    Add Profissional struct and update main function to demonstrate usage

--- exemplos/10-structs/main.go
+++ exemplos/10-structs/main.go
@@ -8,6 +8,12 @@ type Pessoa struct {
 	fumante   bool
 }
 
+type Profissional struct {
+	Pessoa
+	cargo   string
+	salario float64
+}
+
 func main() {
 	pessoa1 := Pessoa{"Alice", "Silva", false}
 	pessoa2 := Pessoa{"Bob", "Santos", true}
@@ -16,8 +22,25 @@ func main() {
 		sobrenome: "Oliveira",
 		fumante:   false,
 	}
+	profissional1 := Profissional{
+		Pessoa:  Pessoa{"Daniel", "Pereira", false},
+		cargo:   "Desenvolvedor",
+		salario: 5000.00,
+	}
 
 	fmt.Println(pessoa1)
 	fmt.Println(pessoa2)
 	fmt.Println(pessoa3)
+	fmt.Println(profissional1)
+
+	// struct anônimos
+	x := struct {
+		titulo  string
+		salario float64
+	}{
+		titulo:  "Desenvolvedor",
+		salario: 5000.00,
+	}
+
+	fmt.Println(x)
 }
